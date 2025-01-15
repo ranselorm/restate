9978130c6503d8f2c77ebfd77b6b2accce418644
ranselorm <ranselorm@outlook.com>
2025-01-15 21:39:14 +0000

    Load fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,27 @@
 import { Text, View } from "react-native";
-import React from "react";
-import { Slot, Stack } from "expo-router";
+import React, { useEffect } from "react";
+import { Slot, SplashScreen, Stack } from "expo-router";
 import "../global.css";
+import { useFonts } from "expo-font";
 
 const RootLayout = () => {
+  const [fontsLoaded] = useFonts({
+    "Rubik-Bold": require("./assets/fonts/Rubik-Bold.ttf"),
+    "Rubik-ExtraBold": require("./assets/fonts/Rubik-ExtraBold.ttf"),
+    "Rubik-Light": require("./assets/fonts/Rubik-Light.ttf"),
+    "Rubik-Medium": require("./assets/fonts/Rubik-Medium.ttf"),
+    "Rubik-Regular": require("./assets/fonts/Rubik-Regular.ttf"),
+    "Rubik-SemiBold": require("./assets/fonts/Rubik-SemiBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
