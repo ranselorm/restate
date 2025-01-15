2c1dfe2f1c75af7240bd379d6ec26f8da2fa9abb
ranselorm <ranselorm@outlook.com>
2025-01-15 21:04:51 +0000

    Add assets

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,10 +1,14 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { Link } from "expo-router";
 
 const Home = () => {
   return (
     <View>
-      <Text>Home</Text>
+      <Text>Home screen</Text>
+      <Link href="/sign-in" className="text-3xl">
+        Sign In
+      </Link>
     </View>
   );
 };
