6de8415f609ff45fb967096ce02c6c82ad15741e
ranselorm <ranselorm@outlook.com>
2025-01-20 21:44:24 +0000

    Finish login

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,26 @@
 import React from "react";
-import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import images from "@/constants/images";
 import icons from "@/constants/icons";
+import { login } from "@/lib/appwrite";
 
 const SignIn = () => {
-  const handleLogin = () => {};
+  const handleLogin = async () => {
+    const result = await login();
+    if (result) {
+      console.log("Login successful");
+    } else {
+      Alert.alert("Failed to login");
+    }
+  };
   return (
     <SafeAreaView className="bg-white h-full">
       <ScrollView contentContainerClassName="h-full">
