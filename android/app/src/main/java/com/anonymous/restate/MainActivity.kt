12c44135a7bb3246c58a609977683bed015384fc
ranselorm <ranselorm@outlook.com>
2025-06-09 23:15:51 +0000

    Refactor project structure and update package names; integrate Firebase services

--- android/app/src/main/java/com/anonymous/restate/MainActivity.kt
+++ android/app/src/main/java/com/anonymous/restate/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.anonymous.testApp
+package com.anonymous.restate
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/anonymous/restate/MainApplication.kt
+++ android/app/src/main/java/com/anonymous/restate/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.anonymous.testApp
+package com.anonymous.restate
 
 import android.app.Application
 import android.content.res.Configuration
