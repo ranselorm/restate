9be6a0f4f7cbd29068b3010a1a560aa277a4ef27
ranselorm <ranselorm@outlook.com>
2025-06-16 19:19:16 +0000

    Refactor

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -6,57 +6,31 @@ import {
   Image,
   TouchableOpacity,
   TextInput,
-  KeyboardAvoidingView,
-  Pressable,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import images from "@/constants/images";
 import icons from "@/constants/icons";
-import { router } from "expo-router";
 
 const SignIn = () => {
-  const handleLogin = async () => {
-    console.log("Login with Google");
-  };
-
   const [isRegister, setIsRegister] = useState(false);
-  // const [toggleLogin, setToggleLogin] = useState(false);
 
   const toggleAuth = () => {
     setIsRegister(!isRegister);
   };
 
+  const handleLogin = async () => {
+    console.log("Logging in...");
+    // Add login logic here
+  };
+
+  const handleRegister = async () => {
+    console.log("Registering...");
+    // Add register logic here
+  };
+
   return (
     <SafeAreaView className="bg-white h-full">
       <ScrollView contentContainerClassName="h-full">
-        <View className="mx-auto"></View>
-
-        {/* <View className="px-10">
-          <Text className="font-rubik uppercase text-center text-base text-black-200">
-            Welcome to Restate
-          </Text>
-          <Text className="font-rubik-bold text-center text-3xl text-black-200 mt-2">
-            Let's Get You Closer To {"\n"}
-            <Text className="text-primary-100">Your Ideal Home</Text>
-          </Text>
-          <Text className="text-center mt-10 font-rubik text-black-200 text-lg ">
-            Login to Restate
-          </Text>
-          <TouchableOpacity
-            onPress={handleLogin}
-            className="bg-white shadow-zinc-300 shadow-md py-2 rounded-full flex-row justify-center items-center w-full gap-x-4 mt-5"
-          >
-            <Image
-              source={icons.google}
-              className="w-5 h-5"
-              resizeMode="contain"
-            />
-            <Text className="text-lg font-rubik-medium text-black-200">
-              Continue with Google
-            </Text>
-          </TouchableOpacity>
-        </View> */}
-        {/* <KeyboardAvoidingView > */}
         <View className="px-10 flex-1 justify-between items-center py-10 pb-20">
           <View className="mt-10">
             <Image
@@ -68,9 +42,10 @@ const SignIn = () => {
               className="text-center font-rubik mt-5"
               style={{ fontSize: 20 }}
             >
-              Login to continue
+              {isRegister ? "Create an account" : "Login to continue"}
             </Text>
           </View>
+
           <View className="w-full">
             <View>
               <Text className="text-base font-rubik text-black-200 font-bold mb-2">
@@ -85,11 +60,11 @@ const SignIn = () => {
                 textContentType="emailAddress"
               />
             </View>
+
             <View>
               <Text className="text-base font-rubik text-black-200 font-bold mb-2 mt-4">
                 Password
               </Text>
-
               <TextInput
                 placeholder="Enter your password"
                 autoCapitalize="none"
@@ -99,44 +74,39 @@ const SignIn = () => {
                 textContentType="password"
               />
             </View>
-            {/* <Pressable className="bg-primary-100 shadow-zinc-300 shadow-md py-3 rounded-md flex-row justify-center items-center w-full gap-x-4 mt-10">
-              <Text>Login</Text>
-            </Pressable> */}
+
             <TouchableOpacity
-              // onPress={() => router.push("/sign-in")}
+              onPress={isRegister ? handleRegister : handleLogin}
               className="bg-primary-100 py-3 rounded-lg justify-center items-center w-full mt-8"
               activeOpacity={1.2}
             >
               <Text className="text-base font-rubik-medium text-white">
-                LOGIN
+                {isRegister ? "SIGN UP" : "LOGIN"}
               </Text>
             </TouchableOpacity>
-            {/* <View className="mt-8"> */}
-            <TouchableOpacity>
-              <Text className="font-rubik-bold mt-5 justify-end text-right">
-                Forgot Password?
-              </Text>
-            </TouchableOpacity>
+
+            {!isRegister && (
+              <TouchableOpacity>
+                <Text className="font-rubik-bold mt-5 justify-end text-right">
+                  Forgot Password?
+                </Text>
+              </TouchableOpacity>
+            )}
+
             <View className="justify-center items-center mt-5">
               <Text className="font-rubik text-lg">or</Text>
 
-              <TouchableOpacity
-                // onPress={() => router.push("/sign-up")}
-                // onPress={() => router.push("/sign-up")}
-                className="mt-5 border-primary-100 border py-3 rounded-lg justify-center items-center w-full flex-row gap-x-4"
-              >
+              <TouchableOpacity className="mt-5 border-primary-100 border py-3 rounded-lg justify-center items-center w-full flex-row gap-x-4">
                 <Image source={icons.google} className="w-5 h-5 mr-3" />
                 <Text className="uppercase items-center justify-center text-base font-rubik-medium text-black-200 flex-row gap-x-4 space-x-4">
-                  {" "}
                   Continue with Google
                 </Text>
               </TouchableOpacity>
+
               <View className="flex-row justify-center items-center mt-5 gap-x-2">
                 <Text className="font-rubik">
-                  {" "}
                   {isRegister ? "Already have an account?" : "Not Registered?"}
                 </Text>
-
                 <TouchableOpacity onPress={toggleAuth} activeOpacity={0.8}>
                   <Text className="font-rubik-bold text-primary-100">
                     {isRegister ? "Login" : "Signup"}
@@ -144,10 +114,8 @@ const SignIn = () => {
                 </TouchableOpacity>
               </View>
             </View>
-            {/* </View> */}
           </View>
         </View>
-        {/* </KeyboardAvoidingView> */}
       </ScrollView>
     </SafeAreaView>
   );
