9bb06e37351b1f0c142c80cc5c3962275ddcb82b
ranselorm <ranselorm@outlook.com>
2025-06-17 21:55:08 +0000

    Remove unused Link import from Home component

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,6 +1,5 @@
 import { View, Text, Pressable } from "react-native";
 import React from "react";
-import { Link } from "expo-router";
 import auth from "@react-native-firebase/auth";
 
 const Home = () => {
